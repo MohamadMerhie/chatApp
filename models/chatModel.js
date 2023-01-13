f55c0b697d16ba5793e4751895b86d5162c16a5e
Amna El Sayed <amnaelsayed2@gmail.com>
2023-01-13 07:56:39 +0100

    InsertedObjectIds

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,12 +1,23 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
-const ChatSchema = new Schema({
-  members: { type: Array },
-  //   members: {
-  //     senderId: { type: String },
-  //     receiverId: { type: String },
-  //   },
-});
+// const ChatSchema = new Schema({
+//   members: { type: Array },
+
+// });
+
+const ChatSchema = new Schema(
+  {
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 const ChatModel = new model("Chat", ChatSchema, "chats");
 export default ChatModel;
--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,10 +1,18 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
-const MessageSchema = new Schema({
-  chatId: { type: String },
-  senderId: { type: String },
-  text: { type: String },
-});
-
+// const MessageSchema = new Schema({
+//   chatId: { type: String },
+//   senderId: { type: String },
+//   text: { type: String },
+// });
+const MessageSchema = new Schema(
+  {
+    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    text: { type: String },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 const MessageModel = model("Message", MessageSchema);
 export default MessageModel;
